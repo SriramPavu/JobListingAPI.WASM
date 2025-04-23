35580172dcce545ef717cc3e48dd0ced24036b5f
SriramPavu <srirampavu11@gmail.com>
2025-04-23 23:31:31 +0200

    Add files via upload

---
+++ Services/ApplicationDBContext.cs
@@ -0,0 +1,14 @@
+﻿using JobListingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JobListingAPI.Services
+{
+    public class ApplicationDBContext : DbContext
+    {
+        public ApplicationDBContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<JobListingDetails> jobListingDetails { get; set; }
+    }
+}
