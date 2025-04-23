002c66651ff33bf9e50d97e12c9aaa737c3cc2e1
SriramPavu <srirampavu11@gmail.com>
2025-04-23 23:35:18 +0200

    Add files via upload

---
+++ Migrations/20250421100900_UpdateJobListingModel.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace JobListingAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateJobListingModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Id",
+                table: "jobListingDetails",
+                newName: "JobId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "JobId",
+                table: "jobListingDetails",
+                newName: "Id");
+        }
+    }
+}
