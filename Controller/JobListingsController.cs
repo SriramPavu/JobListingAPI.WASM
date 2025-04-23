222a4b39daf1666403af3b4f64f1c0f4151b430b
SriramPavu <srirampavu11@gmail.com>
2025-04-23 23:28:11 +0200

    Add files via upload

---
+++ Controller/JobListingsController.cs
@@ -0,0 +1,38 @@
+﻿using JobListingAPI.Models;
+using JobListingAPI.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace JobListingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class JobListingsController : ControllerBase
+    {
+        private readonly ApplicationDBContext _dbContext;
+
+        public JobListingsController(ApplicationDBContext _dbContext)
+        {
+            this._dbContext = _dbContext;
+        }
+        [HttpGet]
+        public ActionResult<List<JobListingDetails>> GetListingDetails()
+        {
+            try
+            {
+                var items = _dbContext.jobListingDetails
+                                      .OrderBy(c => c.JobId)
+                                      .ToList();
+
+                return Ok(items);             
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+
+                return StatusCode(StatusCodes.Status500InternalServerError,
+                                  "Data is not reflected from database");
+            }
+        }
+    }
+}
