25a7c6deb13baf59ce6b83ea688f5d1933c54532
SriramPavu <srirampavu11@gmail.com>
2025-04-23 23:29:03 +0200

    Add files via upload

---
+++ Models/JobListingDetails.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.AspNetCore.Http.HttpResults;
+using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace JobListingAPI.Models
+{
+    public class JobListingDetails
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int JobId { get; set; }
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = "";
+        [Required]
+        [StringLength(100)]
+        public string Company { get; set; } = "";
+        [Required]
+        [StringLength(100)]
+        public string Location { get; set; } = "";
+        [Required]
+        [StringLength(100)]
+        public string Description { get; set; } = "";
+        [Required]
+        public string salary { get; set; } = "";
+        public string JobType { get; set; } = "";
+        public string ContactEmail { get; set; } = "";
+        public DateTime CreatedAt { get; set; } 
+    }
+}
---
+++ Models/JobListingDto.cs
@@ -0,0 +1,14 @@
+﻿namespace JobListingAPI.Models
+{
+    public class JobListingDto
+    {
+        public string Title { get; set; } = "";
+        public string Company { get; set; } = "";
+        public string Location { get; set; } = "";
+        public string Description { get; set; } = "";
+        public string salary { get; set; } = "";
+        public string JobType { get; set; } = "";
+        public string ContactEmail { get; set; } = "";
+        public DateTime CreatedAt { get; set; }
+    }
+}
